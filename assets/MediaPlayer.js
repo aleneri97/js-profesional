62dc82d6cccf418cef72faa723d6edeeac8ad7b9
Manuel Neri <a01329872@itesm.mx>
2021-03-01 15:39:43 -0600

    Plugin: AutoPlay

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,27 +1,49 @@
 // Creamos una "clase" (objeto) llamado Media Player
 // Config será una serie de parámetros incluyendo el video
-function mediaPlayer(config) {
+function MediaPlayer(config) {
 	// Configuramos media igual al video
 	this.media = config.element;
+	this.plugins = config.plugins || [];
+	this._initPlugins();
 }
 
+MediaPlayer.prototype._initPlugins = function () {
+	// debugger;
+	this.plugins.forEach((plugin) => {
+		plugin.run(this);
+	});
+};
+MediaPlayer.prototype.mute = function () {
+	this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+	this.media.muted = false;
+};
+
 // Añadimos la función play mediante el prototipo.
-mediaPlayer.prototype.play = function () {
+MediaPlayer.prototype.play = function () {
 	// lo que hará será que al media (seteado en el constructor), hará play
 	this.media.play();
 };
 
-mediaPlayer.prototype.pause = function () {
+MediaPlayer.prototype.pause = function () {
 	// lo que hará será que al media (seteado en el constructor), hará play
 	this.media.pause();
 };
 
-mediaPlayer.prototype.paused = function () {
+MediaPlayer.prototype.paused = function () {
 	// lo que hará será que al media (seteado en el constructor), hará play
 	return this.media.paused;
 };
-mediaPlayer.prototype.togglePlay = function () {
+MediaPlayer.prototype.togglePlay = function () {
 	this.media.paused ? this.media.play() : this.media.pause();
+	return this.media.paused;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+	this.media.muted = !this.media.muted;
+	return this.media.muted;
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,15 @@
-import MediaPlayer from './MediaPlayer';
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
+const playButton = document.getElementById('play');
+const muteButton = document.getElementById('mute');
 
 // Creamos una instancia de player
-const player = new MediaPlayer({element: video});
+const player = new MediaPlayer({element: video, plugins: [new AutoPlay()]});
 
 // creamos la función de
-button.onclick = () => player.togglePlay();
+playButton.onclick = () =>
+	(playButton.innerHTML = player.togglePlay() ? 'Play' : 'Pause');
+muteButton.onclick = () =>
+	(muteButton.innerHTML = player.toggleMute() ? 'Unmute' : 'Mute');
